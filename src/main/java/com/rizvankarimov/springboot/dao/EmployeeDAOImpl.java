141064c3c78337af5b632bf98ab67c444539ce88
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-12-31 09:42:00 +0100

    created JPA in REST API, added,changed and deleted new Employees

--- src/main/java/com/rizvankarimov/springboot/dao/EmployeeDAOImpl.java
+++ src/main/java/com/rizvankarimov/springboot/dao/EmployeeDAOImpl.java
@@ -2,8 +2,7 @@ package com.rizvankarimov.springboot.dao;
 
 import com.rizvankarimov.springboot.entity.Employee;
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,25 +17,28 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAllEmployees() {
-       Session session = entityManager.unwrap(Session.class);
-       Query<Employee> query = session.createQuery("from Employee", Employee.class);
-       List<Employee> allEmployees = query.getResultList();
 
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> allEmployees = query.getResultList();
          return allEmployees;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
     }
 
     @Override
     public Employee getEmployee(int id) {
-        return null;
+        Employee employee = entityManager.find(Employee.class,id);
+        return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-
+        Query query = entityManager.createQuery("delete from Employee " + "where id =:employeeId");
+        query.setParameter("employeeId",id);
+        query.executeUpdate();
     }
 }
